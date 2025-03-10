d29d0ad23176d3cd69eaa6bfae47a1b172b1f391
Zero_K <adrienkaufman@gmail.com>
2025-03-10 21:25:17 +0100

    some changes

--- Trading/src/Products/Contracts/Options/Option.hpp
+++ Trading/src/Products/Contracts/Options/Option.hpp
@@ -2,7 +2,7 @@
 
 #include <memory>
 
-#include "../Product.hpp"
+#include "../../Product.hpp"
 
 namespace prdt {
 namespace opt {
--- Trading/src/main.cpp
+++ Trading/src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-#include "Products/Options/CallEuropean.hpp"
+#include "Products/Contracts/Options/CallEuropean.hpp"
 
 int main(int argc, char* argv[])
 {
